7981644fac03826c58c109934a929a35a2e02e80
tlgusdl03 <tlgusdl03@gmail.com>
2022-09-14 22:25:43 +0900

    3day

--- Assets/Birdjump.cs
+++ Assets/Birdjump.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Birdjump : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float JumpPower;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * JumpPower;//(0,3)
+        }
     }
 }
